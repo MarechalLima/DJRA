7b8ffd09e215d6628470d238944fef2014b90bfa
Unknown <nicholassowza@hotmail.com>
2017-12-31 15:39:42 -0300

    Changing b_render to background_render

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
-#include "b_render.cpp"
+#include "background_render.hpp"
 
 int main(int argc, char **argv){
 	//image = cvLoadImage(name0, CV_LOAD_IMAGE_COLOR);
 	glGenTextures(1, &textureId);
 	imageBgd = cvLoadImage(nameBgd0, CV_LOAD_IMAGE_COLOR);
-	detMedVisuzalizacao();
+    detMedVisualizacao();
 	initTexture();
+    init(argc,**argv);
 
 	glutMainLoop();
 	return 0;
