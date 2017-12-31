b1901a7441f6bcd6bc0c9dd01c484e7cac001f64
Unknown <nicholassowza@hotmail.com>
2017-12-31 17:29:36 -0300

    Main code

--- main.cpp
+++ main.cpp
@@ -1,13 +1,49 @@
 #include "background_render.hpp"
+#include "render.hpp"
+#include "exit.hpp"
+
+#define WIDTH 600
+#define HEIGHT 600
 
 int main(int argc, char **argv){
-	//image = cvLoadImage(name0, CV_LOAD_IMAGE_COLOR);
-	glGenTextures(1, &textureId);
-	imageBgd = cvLoadImage(nameBgd0, CV_LOAD_IMAGE_COLOR);
-    detMedVisualizacao();
-	initTexture();
-    init(argc,**argv);
+    arqRead = fgetc(arq);
+    if (arqRead == 'r') {
+        nodeUser = NUI_SKELETON_POSITION_HAND_RIGHT;
+    } else if (arqRead == 'l') {
+        nodeUser = NUI_SKELETON_POSITION_HAND_LEFT;
+    } else {
+        printf("Arquivo vazio!\n");
+    }
 
-	glutMainLoop();
-	return 0;
+
+    //HRESULT hr;
+
+    image = cvLoadImage(name0, CV_LOAD_IMAGE_COLOR);
+    imageBgd = cvLoadImage(nameBgd0, CV_LOAD_IMAGE_COLOR);
+
+    if (image == NULL) {
+        printf("Imagem não carregada!\n");
+    }
+
+    //initKinect();
+
+    printf("kinect init");
+
+    //Background generate
+    glGenTextures(1, &textureId);
+    //Background generate
+    //init();
+
+
+    setCamera();
+    glOrtho(0, WIDTH, HEIGHT, 0, 50, -50);
+
+
+    // Main loop
+    glutMainLoop();
+    fclose(arq);
+    return 0;
+    //NuiShutdown();
+
+    return 0;
 }
