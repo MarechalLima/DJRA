7809742674a23989282bdd469a34d398913bbf04
Nícholas Lima de Souza Silva <nicholaslima.rw@gmail.com>
2018-12-29 22:30:53 -0300

    Revert "Removing cmake"
    This reverts commit d0d68c535cd72f94689c961182fd031d89f6de7b.

---
+++ cmake-build-debug/CMakeFiles/djra.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/djra.dir/background_render.cpp.o"
+  "CMakeFiles/djra.dir/main.cpp.o"
+  "CMakeFiles/djra.dir/spheres.cpp.o"
+  "CMakeFiles/djra.dir/render.cpp.o"
+  "CMakeFiles/djra.dir/transition.cpp.o"
+  "CMakeFiles/djra.dir/exit.cpp.o"
+  "CMakeFiles/djra.dir/tutorial.cpp.o"
+  "CMakeFiles/djra.dir/collision.cpp.o"
+  "djra.pdb"
+  "djra"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/djra.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
